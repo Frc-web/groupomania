a5be9698cc6a9e0ee612352aa52a1df0d1b0d14b
Freddy Caron <freddycaron.1@gmail.com>
2021-02-18 16:57:51 +0100

    add confid

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: 'localhost',
+    dialect: 'mysql',
+}); 
+
+// Test DB
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+module.exports = sequelize;
